e72b03f730de0f4a75e578433a3a1006d09b68aa
EmilAhmaBoy <emilahmaboy@gmail.com>
2024-03-25 15:45:20 +0300

    Стартовый коммит

---
+++ src/main/java/org/emilahmaboy/targeted/targeted/client/TargetedClient.java
@@ -0,0 +1,10 @@
+package org.emilahmaboy.targeted.targeted.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TargetedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
